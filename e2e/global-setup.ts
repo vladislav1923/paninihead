c1e963769a15debaa51016ef1adf042b9f9745e9
kozak <kozak@MacBook-Pro-kozak.local>
2026-03-22 21:57:25 +0300

    Another code simplification

--- e2e/global-setup.ts
+++ e2e/global-setup.ts
@@ -1,14 +1,32 @@
 import { execSync } from "node:child_process";
-import { assertE2EDatabaseReady, clearE2EDatabase, loadEnv } from "./postgres";
+import pg from "pg";
+import { config } from "dotenv";
+import { join } from "node:path";
 
 const root = process.cwd();
 
 export default async function globalSetup() {
-  loadEnv();
-  await assertE2EDatabaseReady();
+  config({ path: join(root, ".env.e2e") });
+  const url = process.env.POSTGRES_URL as string;
+  const client = new pg.Client({ connectionString: url });
+  // try e2e database connection
+  try {
+    await client.connect();
+    await client.query("SELECT 1");
+  } catch (cause) {
+    const msg = cause instanceof Error ? cause.message : String(cause);
+    throw new Error(`Cannot connect to the e2e database: ${msg}`);
+  }
+  // run migrations
   execSync("yarn exec prisma migrate deploy", {
     cwd: root,
     stdio: "inherit",
   });
-  await clearE2EDatabase();
+
+  // clear e2e database for pure e2e testing
+  try {
+    await client.query('TRUNCATE TABLE "Collections" CASCADE');
+  } finally {
+    await client.end();
+  }
 }
