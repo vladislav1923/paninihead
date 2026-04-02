bb6e173b818c70c431f004754c7ed291520763bc
kozak <kozak@192.168.1.4>
2026-04-02 12:42:58 +0300

    middleware -> proxy

--- proxy.ts
+++ proxy.ts
@@ -1,10 +1,11 @@
 import type { NextRequest } from "next/server";
 import { authMiddleware } from "@/lib/middlewares/auth";
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   return authMiddleware(request);
 }
 
 export const config = {
   matcher: ["/((?!_next/static|_next/image|favicon.ico|.*\\..*).*)"],
 };
+
